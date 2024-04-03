a2479c00851ee07f6c311152254160ab8fdd1de4
Satyam Srivastava <satyamsrivastava052@gmail.com>
2024-04-03 14:13:04 +0530

    First commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "@/components/MobileNav";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Spider Meets
+        </p>
+      </Link>
+
+      <div className="flex justify-between gap-5 ">
+        {/* Clerk user management */}
+        Clerk User
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
